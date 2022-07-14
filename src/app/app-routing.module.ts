48ebbe95b0b711af2cf8ef40c957622db535817e
udarasan <udarassanjeewa@gmail.com>
2022-07-15 00:35:42 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '',loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
